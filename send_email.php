b3eac8822d6ac820388b6a6b75a7bef6550a16d1
Nemcodespace <nemcodespace@gmail.com>
2024-08-04 20:16:42 -0300

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "suporte@codeflixbr.com.br"; // Substitua pelo seu endereço de email
+    $subject = "Mensagem de Contato de $name";
+    $body = "Nome: $name\nEmail: $email\n\nMensagem:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Email enviado com sucesso!";
+        echo "voltar para página anterior";
+    } else {
+        echo "Falha ao enviar o email.";
+    }
+} else {
+    echo "Método de requisição inválido.";
+}
+?>
